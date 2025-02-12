83eb18bf025be1d3f3cd85d7a6e68bcc882b2d28
DivyanshiGupta-18 <guptadivyanshi756@gmail.com>
2025-02-13 00:05:27 +0530

    first commit l-1 to l-3

---
+++ FirstProgram.java
@@ -0,0 +1,16 @@
+//import java.lang.*;
+//
+//public class FirstProgram{
+//    public static void main(String[] args){
+//        System.out.println("Welcome");
+//        System.out.print("To Java");
+//        System.out.println("L-1");
+//    }
+//}
+
+import java.lang.*;
+    public class FirstProgram{
+        public static void main(String[] args) {
+            System.out.println("Challenge1 Good Morning");
+        }
+    }
