74cfee66222dc75cff3915ab034cb6a1a5dee70a
DivyanshiGupta-18 <guptadivyanshi756@gmail.com>
2025-03-07 00:19:42 +0530

    Added new-folder with example.txt

---
+++ Escape.java
@@ -0,0 +1,11 @@
+public class Escape {
+    public static void main (String[] args){
+        System.out.println("Hello\"KGCoding\"...");
+        System.out.println("Hello\\KGCoding\\..."); /*insert a backslash character in the text at this point*/
+        System.out.println("Hello\tKGCoding..."); /*insert a tab in the text at this point*/
+        System.out.println("Hello\nKGCoding..."); /*insert a newline in the text at this point*/
+        System.out.println("Hello\'KGCoding'..."); /*insert a single quote in the text at this point*/
+        System.out.println("Hello\bKGCoding..."); /*insert a backspace in the text at this point*/
+        System.out.println(" *\n ** \n *** \n **** \n *****");
+    }
+}
--- Variable.java
+++ Variable.java
@@ -1,5 +1,25 @@
 public class Variable {
     public static void main(String[] args) {
-        int a = 45613;
+        int myNumber = 45613;
+        System.out.println(myNumber);
+        int yourNumber;
+
+        yourNumber = 64646;
+        System.out.println(yourNumber);
+
+        boolean isFruit = true;
+        System.out.println(isFruit);
+
+        double myDouble = 26.1161;
+        System.out.println(myDouble);
+
+        float myFloat = 5;
+        System.out.println(myFloat);
+
+        String wishes = "Good Morning";
+        System.out.println(wishes);
+
+        char myCharacter = 'k';
+        System.out.println(myCharacter);
     }
 }
